e8d7d3218c5354129faed23564abad7476d921a4
sandeep-skb <31074448+sandeep-skb@users.noreply.github.com>
2019-05-18 19:28:03 -0700

    Changing nextval to nextnode

--- Python/Linked-list/singly_linked_list.py
+++ Python/Linked-list/singly_linked_list.py
@@ -3,7 +3,7 @@
 class Node:
   def __init__(self, data):
     self.data = data
-    self.nextval = None
+    self.nextnode = None
 
 
 class SLinkedList:
@@ -14,18 +14,19 @@ class SLinkedList:
     node = self.headnode
     while node != None:
       print(node.data)
-      node = node.nextval
+      node = node.nextnode
 
 print("Creating Node:\n <node_name> = Node(<data>)")
-e1 = Node(1)
-e2 = Node(2)
+e1 = Node("monday")
+e2 = Node("teusday")
 print("Linking to the next node:\n node1.nextval = node2")
-e1.nextval = e2
-e3 = Node(3)
-e2.nextval = e3
+e1.nextnode = e2
+e3 = Node("wednesday")
+e2.nextnode = e3
 
 print("Creating a head node for the singly linked list: \n head = SLinkedList()")
 head = SLinkedList()
-print("Assigning node to the head node:\n head.headval = <node>")
+print("Assigning node to the head node:\n head.headval = <node>\n")
 head.headnode = e1
+print("Traversing through the LL:")
 head.print_ll()
