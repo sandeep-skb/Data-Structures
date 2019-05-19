f8f83880e4752376785d7317d9760ec71448d1c3
sandeep-skb <31074448+sandeep-skb@users.noreply.github.com>
2019-05-19 16:33:44 -0700

    creating node and a sample tree

---
+++ Python/Trees/create_node.py
@@ -0,0 +1,32 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+
+root = Node(1) 
+''' following is the tree after above statement 
+        1 
+      /   \ 
+     None  None'''
+  
+root.left      = Node(2); 
+root.right     = Node(3); 
+    
+''' 2 and 3 become left and right children of 1 
+           1 
+         /   \ 
+        2      3 
+     /    \    /  \ 
+   None None None None'''
+  
+  
+root.left.left  = Node(4); 
+'''4 becomes left child of 2 
+           1 
+       /       \ 
+      2          3 
+    /   \       /  \ 
+   4    None  None  None 
+  /  \ 
+None None'''
